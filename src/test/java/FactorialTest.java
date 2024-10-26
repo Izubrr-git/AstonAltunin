a9e1ac593d9a79e562e2c267dc75945d9a43f285
Izubrr-git <izubr863@gmail.com>
2024-10-26 15:35:51 -0300

    First commit

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -1,24 +1,27 @@
 import lesson12_junit_5.Factorial;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FactorialTest {
+    Factorial factorial;
+    @BeforeEach
+    public void setUp() {
+        factorial = new Factorial();
+    }
 
     @Test
     public void testFactorialOfZero() {
-        Factorial factorial = new Factorial();
         assertEquals(1, factorial.calculate(0), "Factorial of 0 should be 1");
     }
 
     @Test
     public void testFactorialOfPositiveNumber() {
-        Factorial factorial = new Factorial();
         assertEquals(120, factorial.calculate(5), "Factorial of 5 should be 120");
     }
 
     @Test
     public void testFactorialOfNegativeNumber() {
-        Factorial factorial = new Factorial();
         Exception exception = assertThrows(IllegalArgumentException.class, () -> factorial.calculate(-1));
         assertEquals("Number must be non-negative.", exception.getMessage());
     }
