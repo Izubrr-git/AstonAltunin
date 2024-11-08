822028b46c87cc0eeaddb0957721a54bcbc5aedd
Izubrr-git <izubr863@gmail.com>
2024-11-08 10:54:33 -0300

    Exercise 10 optimization

--- src/main/java/lesson2/Exercises.java
+++ src/main/java/lesson2/Exercises.java
@@ -122,8 +122,7 @@ public class Exercises {
     public static int[] arrayRevers0and1() {
         int[] array = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         for (int i = 0; i < array.length; i++) {
-            if (array[i] == 0) array[i] = 1;
-            else array[i] = 0;
+            array[i] = 1 - array[i];
         }
         return array;
     }
